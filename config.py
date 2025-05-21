452771b4467d4f3e8789ba1ddad2297e0e5bc38c
Tanjim-Islam <tanjim.islam.riju@g.bracu.ac.bd>
2025-05-21 18:27:48 +0600

    Trying to add finnish language support

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
+import os
+
 ASSETS_DIR = "assets/"
-SCRIPT_FILE = ASSETS_DIR + "script.txt"
-AUDIO_FILE = ASSETS_DIR + "audio.mp3"
+SCRIPT_FILE = os.path.join(ASSETS_DIR, "script.txt")
+AUDIO_FILE = os.path.join(ASSETS_DIR, "audio.mp3")
 
 # Wav2Lip specific paths
 WAV2LIP_MODEL_PATH = "wav2lip/checkpoints/wav2lip_gan.pth"
 
+# TTS Configuration
+TTS_LANGUAGE = "fi" # Default language for Text-to-Speech
+
 # Input/Output files
 FACE_IMAGE_FILE = ASSETS_DIR + "input.jpg"
-OUTPUT_DIR = "output/"
-OUTPUT_VIDEO_FILE = OUTPUT_DIR + "generated_video.mp4"
+OUTPUT_DIR = "output"
+OUTPUT_VIDEO_FILE = os.path.join(OUTPUT_DIR, "generated_video.mp4")
 
 # OUTPUT_DIR, FACE_IMAGE, OUTPUT_VIDEO will be added later 
